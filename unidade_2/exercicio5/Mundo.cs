e599c668b92fea83c9b7cb1d471598c78f1e2319
Jorge Guilherme <jorgekg3@gmail.com>
2019-04-19 17:25:13 -0300

    exercício 5

---
+++ unidade_2/exercicio5/Mundo.cs
@@ -0,0 +1,72 @@
+using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace gcgcg
+{
+  class Mundo
+  {
+    private Ponto4D ptoDirCim = new Ponto4D(100, 100);
+    private Ponto4D ptoOrigem = new Ponto4D(0,0);
+
+    private int directionX1 = 0;
+    private int directionX2 = 100;
+    private int directionY1 = 0;
+    private int directionY2 = -100;
+
+    public void Desenha()
+    {
+      Console.WriteLine("[6] .. Desenha");
+      
+      GL.LineWidth(1);
+      GL.PointSize(4);
+      GL.Color3(Color.Yellow);
+      GL.Begin(PrimitiveType.Lines);
+        GL.Vertex3(this.directionX1, this.directionY1, 0);
+        GL.Vertex3(this.directionX2, this.directionY2, 0);
+      GL.End();
+    }
+
+    public void RightMove() {
+      this.directionX1 += 5;
+      this.directionX2 += 5;
+    }
+
+    public void LeftMove() {
+      this.directionX1 -= 5;
+      this.directionX2 -= 5;
+    }
+
+    public void Height() {
+      this.directionX2 -= 5;
+      this.directionY2 += 5;
+    }
+
+    public void Increase() {
+      this.directionX2 -= 5;
+      this.directionY2 += 5;
+    }
+
+    public void Decrease() {
+      this.directionX2 += 5;
+      this.directionY2 -= 5;
+    }
+
+    public void SRU3D()
+    {
+      Console.WriteLine("[5] .. SRU3D");
+
+      GL.LineWidth(1);
+      GL.Begin(PrimitiveType.Lines);
+      GL.Color3(Color.Red);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(200, 0, 0);
+      GL.Color3(Color.Green);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, -200, 0);
+      GL.Color3(Color.Blue);
+      GL.Vertex3(0, 0, 0); GL.Vertex3(0, 0, 200);
+      GL.End();
+    }
+  }
+
+}
---
+++ unidade_2/exercicio5/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace gcgcg
+{
+  class Render : GameWindow
+  {
+    Mundo mundo = new Mundo();
+    public Render(int width, int height) : base(width, height) { }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      Console.WriteLine("[2] .. OnLoad");
+    }
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+      base.OnUpdateFrame(e);
+      Console.WriteLine("[3] .. OnUpdateFrame");
+
+      OpenTK.Input.KeyboardState keyboardState = OpenTK.Input.Keyboard.GetState();
+
+      KeyPress(keyboardState);
+
+      GL.MatrixMode(MatrixMode.Projection);
+      GL.LoadIdentity();
+      GL.Ortho(-400, 400, 400, -400, -1, 1);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+      base.OnRenderFrame(e);
+      Console.WriteLine("[4] .. OnRenderFrame");
+      
+      GL.Clear(ClearBufferMask.ColorBufferBit);
+      GL.ClearColor(Color.Gray);
+      GL.MatrixMode(MatrixMode.Modelview);
+
+      mundo.SRU3D();
+      mundo.Desenha();
+
+      this.SwapBuffers();
+    }
+
+    public void KeyPress(OpenTK.Input.KeyboardState keyState)
+    {
+      if (keyState.IsKeyDown(OpenTK.Input.Key.W)) {
+        mundo.RightMove();
+      }
+      if (keyState.IsKeyDown(OpenTK.Input.Key.Q)) {
+        mundo.LeftMove();
+      }
+      if (keyState.IsKeyDown(OpenTK.Input.Key.Z)) {
+        mundo.Increase();
+      }
+      if (keyState.IsKeyDown(OpenTK.Input.Key.A)) {
+        mundo.Decrease();
+      }
+    }
+    
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("[1] .. Main");
+
+      Render window = new Render(800, 800);
+      window.Run();
+      window.Run(1.0/60.0);
+    }
+  }
+}
