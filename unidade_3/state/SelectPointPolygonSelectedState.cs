9585651bf08f39e45650da84f19a467454a440e0
Jorge Guilherme <jorgekg3@gmail.com>
2019-05-10 20:29:18 -0300

    ajustes

--- unidade_3/state/SelectPointPolygonSelectedState.cs
+++ unidade_3/state/SelectPointPolygonSelectedState.cs
@@ -9,23 +9,35 @@ namespace gcgcg
 
     public IState Perform(Command command, Mundo mundo)
     {
-      if (command.Equals(Command.SELECT_VERTEX)) {
-        mundo.pointSelected = new Ponto4D() {
-          X = Mouse.X, Y = Mouse.Y
+      if (command.Equals(Command.SELECT_VERTEX))
+      {
+        mundo.pointSelected = new Ponto4D()
+        {
+          X = Mouse.X,
+          Y = Mouse.Y
         };
         return this;
-      } else if (command.Equals(Command.MOUSE_MOVE)) {
-        Ponto4D point4D = mundo.polygonSelected.pointSelected;
-        if (point4D != null) {
-          point4D.X = Mouse.X;
-          point4D.Y = Mouse.Y;
-        }
-        mundo.polygonSelected.Bbox = null;
+      }
+      else if (command.Equals(Command.MOUSE_MOVE))
+      {
+        UpdateVertex(mundo);
         return this;
-      } else {
+      }
+      else
+      {
         mundo.pointSelected = null;
       }
       return new MainState();
+    }
+    private void UpdateVertex(Mundo mundo)
+    {
+      Ponto4D point4D = mundo.polygonSelected.pointSelected;
+      if (point4D != null)
+      {
+        point4D.X = Mouse.X;
+        point4D.Y = Mouse.Y;
+      }
+      mundo.polygonSelected.Bbox = null;
     }
   }
 }
